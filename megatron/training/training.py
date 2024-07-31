ef4b8d0deb7435a234ce31a24628e6bab6cdc233
Lyuwen Fu <fulvwen@gmail.com>
2024-07-31 16:20:19 +0800

    add extra validation for the do-valid only pass

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -312,6 +312,24 @@ def pretrain(train_valid_test_dataset_provider,
                                    valid_data_iterator, model,
                                    iteration, process_non_loss_data_func, config,
                                    verbose=True, write_to_tensorboard=not args.skip_train)
+        # Extra validations
+        if extra_valid_data_iterators:
+            for i, extra_valid_data_iterator in enumerate(extra_valid_data_iterators):
+                num_samples = args.extra_valid_data_samples[i]
+                data_name = args.extra_valid_data_names[i]
+                print_rank_0(f">>>> {i=} {extra_valid_data_iterator=} {num_samples=} {data_name=}")
+                eval_iters = num_samples // args.global_batch_size
+                old_eval_iters = args.eval_iters
+                args.eval_iters = eval_iters
+                # Run evaluation for extra datasets
+                evaluate_and_print_results(prefix, forward_step_func,
+                                           extra_valid_data_iterator, model,
+                                           iteration, process_non_loss_data_func,
+                                           config, False,
+                                           group_prefix=data_name,
+                                           )
+                # Reset iter count
+                args.eval_iters = old_eval_iters
 
     if args.do_test:
         prefix = f'iteration {iteration} on test set'
