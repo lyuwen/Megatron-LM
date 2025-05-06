1d9dbe746fb94b6a13ad667cd24ece44300bbea9
pumpkinsm <123sssmmm@gmail.com>
2025-05-06 10:29:16 +0800

    fix fp8 setting

--- megatron/core/transformer/moe/moe_layer.py
+++ megatron/core/transformer/moe/moe_layer.py
@@ -181,7 +181,7 @@ class MoELayer(BaseMoELayer):
             (dispatched_input, tokens_per_expert, permuted_probs) = (
                 self.token_dispatcher.token_permutation(hidden_states, probs, routing_map)
             )
-            use_experts_fp8_context = self.config.v3_fp8_grouped_linear and self.config.fp8_recipe == Fp8Recipe.blockwise
+            use_experts_fp8_context = self.config.v3_fp8_grouped_linear
             experts_fp8_context = get_fp8_context(self.config) if use_experts_fp8_context else nullcontext()
             with experts_fp8_context:
                 expert_output, mlp_bias = self.experts(
@@ -199,7 +199,7 @@ class MoELayer(BaseMoELayer):
             (dispatched_input, tokens_per_expert, permuted_probs) = self.token_dispatcher.token_permutation(
                 hidden_states, probs, routing_map
             )
-            use_experts_fp8_context = self.config.v3_fp8_grouped_linear and self.config.fp8_recipe == Fp8Recipe.blockwise
+            use_experts_fp8_context = self.config.v3_fp8_grouped_linear
             experts_fp8_context = get_fp8_context(self.config) if use_experts_fp8_context else nullcontext()
             with experts_fp8_context:
                 if FP8_CTX_MOE:
