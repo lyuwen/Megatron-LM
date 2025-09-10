bug fixes, switch to tp aware entropy calculation and add entropy to tensorboard

megatron/core/models/gpt/gpt_model.py
@@ -433,7 +433,8 @@ class GPTModel(LanguageModule):
             return logits.transpose(0, 1).contiguous()
 
         # TODO calculate entropy
-        entropy = _entropy.entropy_from_logits(logits.clone().detach())
+        #  entropy = _entropy.entropy_from_logits(logits.clone().detach())
+        entropy = _entropy.vocab_parallel_entropy(logits.clone().detach())
         entropy = torch.sum(entropy * loss_mask, dim=-1) / torch.sum(loss_mask, dim=-1)
         _entropy.push_micro_batch(entropy)
 

megatron/core/rl/entropy.py
@@ -29,8 +29,8 @@ _ENTROPY_STORE = []
 
 
 def push_micro_batch(item):
-    global = _ENTROPY_STORE
-    _ENTROPY_STORE.append(_ENTROPY_STORE)
+    global _ENTROPY_STORE
+    _ENTROPY_STORE.append(item)
 
 
 def get_batch():
@@ -44,7 +44,7 @@ def get_batch():
 
 def get_batch_entropy():
     seq_entropy = get_batch()
-    if not seq_entropy:
+    if seq_entropy is not None:
         return None
     entropy = torch.mean(seq_entropy)  # seq-mean
     return entropy